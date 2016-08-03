1b6c6d3abd7f9d8df724131a7c04fe55029cfda8
Oliver Eder <eder@essential-data.sk>
2016-08-03 21:20:42 +0200

    Added logging into BigramDataListGeneratorImpl.

--- src/main/java/com/trainingdataprocessor/bigrams/BigramDataListGeneratorImpl.java
+++ src/main/java/com/trainingdataprocessor/bigrams/BigramDataListGeneratorImpl.java
@@ -3,11 +3,15 @@ package com.trainingdataprocessor.bigrams;
 import com.trainingdataprocessor.cache.ConstantTagsCache;
 import com.trainingdataprocessor.data.BigramData;
 import com.trainingdataprocessor.database.TrainingDataAccessor;
+import com.trainingdataprocessor.subpaths.StartTagEndTagPairsGeneratorImpl;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class BigramDataListGeneratorImpl implements BigramDataListGenerator {
+
+    private final static Logger LOGGER = Logger.getLogger(BigramDataListGeneratorImpl.class.getName());
 
     private ConstantTagsCache constantTagsCache;
 
@@ -22,6 +26,9 @@ public class BigramDataListGeneratorImpl implements BigramDataListGenerator {
     }
 
     public List<BigramData> generate(List<String> tags) {
+        LOGGER.info("ENTERING generate method of BigramDataListGeneratorImpl... ");
+        LOGGER.info("*********************************************************************");
+        long startTime = System.currentTimeMillis();
         List<BigramData> bigramDataList = new ArrayList<BigramData>();
         for (int i = 0; i < tags.size() - 1; i++) {
             String tag1 = tags.get(i);
@@ -33,8 +40,18 @@ public class BigramDataListGeneratorImpl implements BigramDataListGenerator {
             trainingDataAccessor.populateBigramTag1FrequencyData(bigramData);
             double bigramProbability = bigramProbabilityCalculator.calculate(bigramData.getBigramFrequency(), bigramData.getTag1Frequency());
             bigramData.setBigramProbability(bigramProbability);
+
+            LOGGER.info("Processing bigram -> tag1: " + tag1 + ", tag2: " + tag2 + ", " + "isTag1Constant: " + isTag1Constant +
+                    ", isTag2Constant: " + isTag2Constant + ", bigramProbability: " + bigramProbability + ", bigramFrequency: " +
+                    bigramData.getBigramFrequency() + ", tag1Frequency: " + bigramData.getTag1Frequency());
+
             bigramDataList.add(bigramData);
         }
+        long stopTime = System.currentTimeMillis();
+        long elapsedTime = stopTime - startTime;
+        LOGGER.info("BigramDataList with size " + bigramDataList.size() + " was created in " + elapsedTime + " miliseconds.");
+        LOGGER.info("LEAVING generate method of BigramDataListGeneratorImpl... ");
+        LOGGER.info("*********************************************************************");
         return bigramDataList;
     }
 }
