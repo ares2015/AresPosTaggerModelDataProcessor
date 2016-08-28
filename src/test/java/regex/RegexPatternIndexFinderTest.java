2c12b75ed05c22d6ecd2e2970bd0c70faaa0ff4e
Oliver Eder <eder@essential-data.sk>
2016-08-28 14:10:28 +0200

    Fixed RegexPatternIndexFinderTest. Regex was wrong for IS relationship.

--- src/test/java/regex/RegexPatternIndexFinderTest.java
+++ src/test/java/regex/RegexPatternIndexFinderTest.java
@@ -18,18 +18,18 @@ public class RegexPatternIndexFinderTest {
 
     @Test
     public void testFind(){
-        String encodedPath = "XXXNJJINXXXXXDNNIN";
-        String regexPattern =  "N[NJD]+IN";
+        String encodedPath = "NNIJNNXXJNINNN";
+        String regexPattern =  "[NJD]+NI[NJD]+N";
         List<RegexPatternIndexData> regexPatternIndexFinderList = regexPatternIndexFinder.find(encodedPath, regexPattern);
         assertEquals(2, regexPatternIndexFinderList.size());
 
-        assertEquals(3, regexPatternIndexFinderList.get(0).getStartIndex());
-        assertEquals(7, regexPatternIndexFinderList.get(0).getEndIndex() - 1);
-        assertEquals("NJJIN", regexPatternIndexFinderList.get(0).getPattern());
+        assertEquals(0, regexPatternIndexFinderList.get(0).getStartIndex());
+        assertEquals(5, regexPatternIndexFinderList.get(0).getEndIndex() - 1);
+        assertEquals("NNIJNN", regexPatternIndexFinderList.get(0).getPattern());
 
-        assertEquals(14, regexPatternIndexFinderList.get(1).getStartIndex());
-        assertEquals(17, regexPatternIndexFinderList.get(1).getEndIndex() - 1);
-        assertEquals("NNIN", regexPatternIndexFinderList.get(1).getPattern());
+        assertEquals(8, regexPatternIndexFinderList.get(1).getStartIndex());
+        assertEquals(13, regexPatternIndexFinderList.get(1).getEndIndex() - 1);
+        assertEquals("JNINNN", regexPatternIndexFinderList.get(1).getPattern());
 
     }
 }
