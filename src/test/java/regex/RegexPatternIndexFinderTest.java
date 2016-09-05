a5994c3d446862d3d19251555b99c46dc912996e
Oliver Eder <eder@essential-data.sk>
2016-09-05 23:15:56 +0200

    Extended RegexPatternIndexFinderTest. More regexes successfully tested.

--- src/test/java/regex/RegexPatternIndexFinderTest.java
+++ src/test/java/regex/RegexPatternIndexFinderTest.java
@@ -17,19 +17,60 @@ public class RegexPatternIndexFinderTest {
     private RegexPatternIndexFinder regexPatternIndexFinder = new RegexPatternIndexFinderImpl();
 
     @Test
-    public void testFind(){
-        String encodedPath = "NNI#JNPN$N";
-        String regexPattern =  "[NJD]*NI[#NJD]*[N$][P]?[NJD$]?";
-        List<RegexPatternIndexData> regexPatternIndexFinderList = regexPatternIndexFinder.find(encodedPath, regexPattern);
+    public void testFindOK(){
+        //
+        String regexPattern =  "[NJD]*N[A]?I[#NJD$]*[PT]?[Y#NJD$V]*[PT]?[Y#NJD$V]*";
+        List<RegexPatternIndexData> regexPatternIndexFinderList = null;
+        String encodedPath = null;
+//        New York City is the largest city in the United States - NNIDJNPDNN
+        encodedPath = "NNIDJNPDNN";
+        regexPatternIndexFinderList = regexPatternIndexFinder.find(encodedPath, regexPattern);
         assertEquals(1, regexPatternIndexFinderList.size());
+        System.out.println(regexPatternIndexFinderList.get(0).getPattern());
+        //New York City was settled by Europeans from The Netherlands in 1624 - NNNI$PNPDNP#
+        encodedPath = "NNNI$PNPDNP#";
+        regexPatternIndexFinderList = regexPatternIndexFinder.find(encodedPath, regexPattern);
+        assertEquals(1, regexPatternIndexFinderList.size());
+        System.out.println(regexPatternIndexFinderList.get(0).getPattern());
+        //Subway transportation is provided by the New York City Subway system - NNI$PDNNNN
+        encodedPath = "NNI$PDNNNN";
+        regexPatternIndexFinderList = regexPatternIndexFinder.find(encodedPath, regexPattern);
+        assertEquals(1, regexPatternIndexFinderList.size());
+        System.out.println(regexPatternIndexFinderList.get(0).getPattern());
+        //The city is divided into five major parts called boroughs - DNI$P#JN$N
+        encodedPath = "DNI$P#JN$N";
+        regexPatternIndexFinderList = regexPatternIndexFinder.find(encodedPath, regexPattern);
+        assertEquals(1, regexPatternIndexFinderList.size());
+        System.out.println(regexPatternIndexFinderList.get(0).getPattern());
+//        Billy Graham was born on a dairy farm near Charlotte - NNI$PDJNPN
+        encodedPath = "NNI$PDJNPN";
+        regexPatternIndexFinderList = regexPatternIndexFinder.find(encodedPath, regexPattern);
+        assertEquals(1, regexPatternIndexFinderList.size());
+        System.out.println(regexPatternIndexFinderList.get(0).getPattern());
+        //Vivaldi probably was taught to play the violin by father - NAI$TVDNPN
+        encodedPath = "NAI$TVDNPYN";
+        regexPatternIndexFinderList = regexPatternIndexFinder.find(encodedPath, regexPattern);
+        assertEquals(1, regexPatternIndexFinderList.size());
+        System.out.println(regexPatternIndexFinderList.get(0).getPattern());
+        encodedPath = "NIN";
+        regexPatternIndexFinderList = regexPatternIndexFinder.find(encodedPath, regexPattern);
+        assertEquals(1, regexPatternIndexFinderList.size());
+        System.out.println(regexPatternIndexFinderList.get(0).getPattern());
+        encodedPath = "NIYN";
+        regexPatternIndexFinderList = regexPatternIndexFinder.find(encodedPath, regexPattern);
+        assertEquals(1, regexPatternIndexFinderList.size());
+        System.out.println(regexPatternIndexFinderList.get(0).getPattern());
+    }
 
-//        assertEquals(0, regexPatternIndexFinderList.get(0).getStartIndex());
-//        assertEquals(5, regexPatternIndexFinderList.get(0).getEndIndex() - 1);
-//        assertEquals("NNIJNN", regexPatternIndexFinderList.get(0).getPattern());
-//
-//        assertEquals(8, regexPatternIndexFinderList.get(1).getStartIndex());
-//        assertEquals(13, regexPatternIndexFinderList.get(1).getEndIndex() - 1);
-//        assertEquals("JNINNN", regexPatternIndexFinderList.get(1).getPattern());
+    @Test
+    public void testFindNOK(){
+        String regexPattern =  "[NJD]*N[A]?I[#NJD$]*[PT]?[Y#NJD$V]*[PT]?[Y#NJD$V]*";
+        List<RegexPatternIndexData> regexPatternIndexFinderList = null;
+        String encodedPath = null;
+        encodedPath = "NNVN";
+        regexPatternIndexFinderList = regexPatternIndexFinder.find(encodedPath, regexPattern);
+        assertEquals(0, regexPatternIndexFinderList.size());
+
 
     }
 }
