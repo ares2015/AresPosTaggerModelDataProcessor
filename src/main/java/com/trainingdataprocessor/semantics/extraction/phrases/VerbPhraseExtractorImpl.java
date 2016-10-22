22175da7f0f61b9b3ba6a6ff6050a392339a488a
Oliver Eder <eder@essential-data.sk>
2016-10-22 13:49:35 +0200

    fix problem

---
+++ src/main/java/com/trainingdataprocessor/semantics/extraction/phrases/VerbPhraseExtractorImpl.java
@@ -0,0 +1,7 @@
+package com.trainingdataprocessor.semantics.extraction.phrases;
+
+/**
+ * Created by Oliver on 10/22/2016.
+ */
+public class VerbPhraseExtractorImpl {
+}
---
+++ src/test/java/semantics/extraction/VerbPhraseExtractorTest.java
@@ -0,0 +1,7 @@
+package semantics.extraction;
+
+/**
+ * Created by Oliver on 10/22/2016.
+ */
+public class VerbPhraseExtractorTest {
+}
