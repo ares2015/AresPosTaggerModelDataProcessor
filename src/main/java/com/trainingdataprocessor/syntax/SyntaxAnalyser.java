78dff5ebdc11852be35f2cb9310030652f7bc9a3
Oliver Eder <eder@essential-data.sk>
2016-11-09 21:11:10 +0100

    SyntaxAnalyser implementation prepared.

---
+++ src/main/java/com/trainingdataprocessor/syntax/SyntaxAnalyser.java
@@ -0,0 +1,9 @@
+package com.trainingdataprocessor.syntax;
+
+/**
+ * Created by Oliver on 11/9/2016.
+ */
+public interface SyntaxAnalyser {
+
+    void analyse();
+}
---
+++ src/main/java/com/trainingdataprocessor/syntax/SyntaxAnalyserImpl.java
@@ -0,0 +1,28 @@
+package com.trainingdataprocessor.syntax;
+
+import com.trainingdataprocessor.data.TestDataRow;
+import com.trainingdataprocessor.database.TrainingDataAccessor;
+
+import java.util.List;
+
+/**
+ * Created by Oliver on 11/9/2016.
+ */
+public class SyntaxAnalyserImpl implements SyntaxAnalyser, Runnable {
+
+    private TrainingDataAccessor trainingDataAccessor;
+
+    private List<TestDataRow> testDataRowList;
+
+
+
+    @Override
+    public void analyse() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
