3be2d9386e6e462be1a11ad1335e4ea6f8309d02
oliver.eder <oliver.eder@avitech.aero>
2017-01-17 14:07:51 +0100

    Added new tags.

--- src/main/java/com/trainingdataprocessor/cache/TagsCodingCache.java
+++ src/main/java/com/trainingdataprocessor/cache/TagsCodingCache.java
@@ -18,18 +18,26 @@ public class TagsCodingCache {
     static {
         encodingMap.put(Tags.ADJECTIVE, EncodedTags.ADJECTIVE);
         encodingMap.put(Tags.ADVERB, EncodedTags.ADVERB);
+        encodingMap.put(Tags.AND_OR, EncodedTags.AND_OR);
         encodingMap.put(Tags.CONJUNCTION, EncodedTags.CONJUNCTION);
         encodingMap.put(Tags.DETERMINER, EncodedTags.DETERMINER);
+        encodingMap.put(Tags.DO, EncodedTags.DO);
+        encodingMap.put(Tags.FOREIGN_WORD, EncodedTags.FOREIGN_WORD);
         encodingMap.put(Tags.HAVE, EncodedTags.HAVE);
         encodingMap.put(Tags.HERE, EncodedTags.HERE);
         encodingMap.put(Tags.IS_ARE, EncodedTags.IS_ARE);
+        encodingMap.put(Tags.INTERJECTION, EncodedTags.INTERJECTION);
+        encodingMap.put(Tags.LIST_ITEM_MARKER, EncodedTags.LIST_ITEM_MARKER);
         encodingMap.put(Tags.MODAL_VERB, EncodedTags.MODAL_VERB);
         encodingMap.put(Tags.NOT, EncodedTags.NOT);
         encodingMap.put(Tags.NOUN, EncodedTags.NOUN);
         encodingMap.put(Tags.NUMBER, EncodedTags.NUMBER);
+        encodingMap.put(Tags.PARTICLE, EncodedTags.PARTICLE);
+        encodingMap.put(Tags.PREDETERMINER, EncodedTags.PREDETERMINER);
         encodingMap.put(Tags.PREPOSITION, EncodedTags.PREPOSITION);
         encodingMap.put(Tags.PRONOUN_PERSONAL, EncodedTags.PRONOUN_PERSONAL);
         encodingMap.put(Tags.PRONOUN_POSSESIVE, EncodedTags.PRONOUN_POSSESIVE);
+        encodingMap.put(Tags.SYMBOL, EncodedTags.SYMBOL);
         encodingMap.put(Tags.QUANTIFIER, EncodedTags.QUANTIFIER);
         encodingMap.put(Tags.THERE, EncodedTags.THERE);
         encodingMap.put(Tags.TO, EncodedTags.TO);
@@ -40,22 +48,29 @@ public class TagsCodingCache {
         encodingMap.put(Tags.WH_DETERMINER, EncodedTags.WH_DETERMINER);
         encodingMap.put(Tags.WH_PRONOUN, EncodedTags.WH_PRONOUN);
         encodingMap.put(Tags.WH_PRONOUN_POSSESSIVE, EncodedTags.WH_PRONOUN_POSSESSIVE);
-        encodingMap.put(Tags.AND_OR, EncodedTags.AND_OR);
 
         decodingMap.put(EncodedTags.ADJECTIVE, Tags.ADJECTIVE);
         decodingMap.put(EncodedTags.ADVERB, Tags.ADVERB);
+        decodingMap.put(EncodedTags.AND_OR, Tags.AND_OR);
         decodingMap.put(EncodedTags.CONJUNCTION, Tags.CONJUNCTION);
         decodingMap.put(EncodedTags.DETERMINER, Tags.DETERMINER);
+        decodingMap.put(EncodedTags.DO, Tags.DO);
+        decodingMap.put(EncodedTags.FOREIGN_WORD, Tags.FOREIGN_WORD);
         decodingMap.put(EncodedTags.HAVE, Tags.HAVE);
         decodingMap.put(EncodedTags.HERE, Tags.HERE);
         decodingMap.put(EncodedTags.IS_ARE, Tags.IS_ARE);
+        decodingMap.put(EncodedTags.INTERJECTION, Tags.INTERJECTION);
+        decodingMap.put(EncodedTags.LIST_ITEM_MARKER, Tags.LIST_ITEM_MARKER);
         decodingMap.put(EncodedTags.MODAL_VERB, Tags.MODAL_VERB);
         decodingMap.put(EncodedTags.NOT, Tags.NOT);
         decodingMap.put(EncodedTags.NOUN, Tags.NOUN);
         decodingMap.put(EncodedTags.NUMBER, Tags.NUMBER);
+        decodingMap.put(EncodedTags.PARTICLE, Tags.PARTICLE);
+        decodingMap.put(EncodedTags.PREDETERMINER, Tags.PREDETERMINER);
         decodingMap.put(EncodedTags.PREPOSITION, Tags.PREPOSITION);
         decodingMap.put(EncodedTags.PRONOUN_PERSONAL, Tags.PRONOUN_PERSONAL);
         decodingMap.put(EncodedTags.PRONOUN_POSSESIVE, Tags.PRONOUN_POSSESIVE);
+        decodingMap.put(EncodedTags.SYMBOL, Tags.SYMBOL);
         decodingMap.put(EncodedTags.QUANTIFIER, Tags.QUANTIFIER);
         decodingMap.put(EncodedTags.THERE, Tags.THERE);
         decodingMap.put(EncodedTags.TO, Tags.TO);
@@ -66,7 +81,6 @@ public class TagsCodingCache {
         decodingMap.put(EncodedTags.WH_DETERMINER, Tags.WH_DETERMINER);
         decodingMap.put(EncodedTags.WH_PRONOUN, Tags.WH_PRONOUN);
         decodingMap.put(EncodedTags.WH_PRONOUN_POSSESSIVE, Tags.WH_PRONOUN_POSSESSIVE);
-        decodingMap.put(EncodedTags.AND_OR, Tags.AND_OR);
     }
 
 }
--- src/main/java/com/trainingdataprocessor/tags/EncodedTags.java
+++ src/main/java/com/trainingdataprocessor/tags/EncodedTags.java
@@ -5,27 +5,61 @@ package com.trainingdataprocessor.tags;
  */
 public final class EncodedTags {
 
-    public static final String NUMBER = "#";
+    public static final String ADJECTIVE = "J";
+
+    public static final String ADVERB = "A";
+
+    public static final String AND_OR = ">";
+
+    public static final String CONJUNCTION = "U";
+
+    public static final String DETERMINER = "D";
+
+    public static final String DO = "L";
+
+    public static final String FOREIGN_WORD = "1";
+
+    public static final String HAVE = "H";
+
+    public static final String HERE = "G";
+
+    public static final String IS_ARE = "I";
+
+    public static final String INTERJECTION = "2";
+
+    public static final String LIST_ITEM_MARKER = "3";
+
+    public static final String MODAL_VERB = "M";
+
+    public static final String NOT = "O";
 
     public static final String NOUN = "N";
+
+    public static final String NUMBER = "#";
+
+    public static final String PARTICLE = "4";
+
+    public static final String PREDETERMINER = "5";
+
+    public static final String PREPOSITION = "P";
+
+    public static final String PRONOUN_PERSONAL = "R";
+
+    public static final String PRONOUN_POSSESIVE = "S";
+
+    public static final String SYMBOL = "6";
+
+    public static final String QUANTIFIER = "Q";
+
+    public static final String THERE = "Z";
+
+    public static final String TO = "T";
 
     public static final String VERB = "V";
 
     public static final String VERB_ING = "@";
 
     public static final String VERB_ED = "$";
-
-    public static final String MODAL_VERB = "M";
-
-    public static final String TO = "T";
-
-    public static final String NOT = "O";
-
-    public static final String HAVE = "H";
-
-    public static final String PRONOUN_PERSONAL = "R";
-
-    public static final String PRONOUN_POSSESIVE = "S";
 
     public static final String WH_DETERMINER = "W";
 
@@ -34,28 +68,6 @@ public final class EncodedTags {
     public static final String WH_PRONOUN_POSSESSIVE = "Y";
 
     public static final String WH_ADVERB = "B";
-
-    public static final String ADJECTIVE = "J";
-
-    public static final String ADVERB = "A";
-
-    public static final String PREPOSITION = "P";
-
-    public static final String CONJUNCTION = "U";
-
-    public static final String DETERMINER = "D";
-
-    public static final String QUANTIFIER = "Q";
-
-    public static final String IS_ARE = "I";
-
-    public static final String THERE = "Z";
-
-    public static final String HERE = "G";
-
-    public static final String AND_OR = ">";
-
-    public static final String DO = "L";
 
 
 }
